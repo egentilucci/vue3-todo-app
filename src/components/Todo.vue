c6dcfdd0ec660498bb9b8a54aca5b02b35140676
egentilucci <elia.gentilucci@gmail.com>
2023-12-02 23:43:51 +0100

    feat: create Todo component

---
+++ src/components/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+  <input type="checkbox" :checked="todo.completed" @click="$emit('updateTodoStatus', index)" />
+  <span :class="{ done: todo.completed }">{{ todo.title }}</span>
+  <button @click="$emit('removeTodo', index)">Remove</button>
+</template>
+
+<script lang="ts">
+import type { TodoComment } from 'typescript'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    todo: {
+      type: Object
+    },
+    index: {
+      type: Number
+    }
+  },
+  emits: ['removeTodo', 'updateTodoStatus'],
+  setup() {}
+})
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
