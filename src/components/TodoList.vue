f5adce6e28c53a0d3846280e1786e4a60b409110
egentilucci <elia.gentilucci@gmail.com>
2023-12-02 23:43:40 +0100

    feat: create TodoList component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-show="uncompletedTodos">TO DO [{{ uncompletedTodos }}]</div>
+  <span v-for="(todo, index) in todoList">
+    <div v-if="todo.completed === false">
+      <Todo
+        :todo="todo"
+        :index="index"
+        @updateTodoStatus="updateTodoStatus"
+        @removeTodo="removeTodo"
+      />
+    </div>
+  </span>
+
+  <div v-show="completedTodos">DONE [{{ completedTodos }}]</div>
+  <span v-for="(todo, index) in todoList">
+    <div v-if="todo.completed === true">
+      <Todo
+        :todo="todo"
+        :index="index"
+        @updateTodoStatus="updateTodoStatus"
+        @removeTodo="removeTodo"
+      />
+    </div>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import Todo from './Todo.vue'
+
+export default defineComponent({
+  props: {
+    todoList: {
+      type: Array
+    }
+  },
+  components: { Todo },
+  setup(props) {
+    const completedTodos = computed(() => {
+      return props.todoList.filter((e) => e.completed === true).length
+    })
+
+    const uncompletedTodos = computed(() => {
+      return props.todoList.filter((e) => e.completed === false).length
+    })
+
+    const removeTodo = (index: number) => {
+      console.log(`${props.todoList[index].title} removed from todoList`)
+
+      props.todoList.splice(index, 1)
+    }
+
+    const updateTodoStatus = (index: number) => {
+      props.todoList[index].completed = !props.todoList[index].completed
+    }
+
+    return {
+      completedTodos,
+      uncompletedTodos,
+      removeTodo,
+      updateTodoStatus
+    }
+  }
+})
+</script>
